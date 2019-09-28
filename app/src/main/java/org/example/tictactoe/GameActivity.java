bc15c9c38fef8068ec0edfe5e79995d99a1707d2
ynws <ynw_soft@yahoo.co.jp>
2019-09-28 22:41:32 +0900

    chapter 6 BGM

--- app/src/main/java/org/example/tictactoe/GameActivity.java
+++ app/src/main/java/org/example/tictactoe/GameActivity.java
@@ -12,21 +12,24 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 
 public class GameActivity extends Activity {
-   // ...
    public static final String KEY_RESTORE = "key_restore";
    public static final String PREF_RESTORE = "pref_restore";
+   private MediaPlayer mMediaPlayer;
+   private Handler mHandler = new Handler();
+   // ...
    private GameFragment mGameFragment;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_game);
-      // Restore game here...
       mGameFragment = (GameFragment) getFragmentManager()
             .findFragmentById(R.id.fragment_game);
       boolean restore = getIntent().getBooleanExtra(KEY_RESTORE, false);
@@ -46,6 +49,11 @@ public class GameActivity extends Activity {
 
    public void reportWinner(final Tile.Owner winner) {
       AlertDialog.Builder builder = new AlertDialog.Builder(this);
+      if (mMediaPlayer != null && mMediaPlayer.isPlaying()) {
+         mMediaPlayer.stop();
+         mMediaPlayer.reset();
+         mMediaPlayer.release();
+      }
       builder.setMessage(getString(R.string.declare_winner, winner));
       builder.setCancelable(false);
       builder.setPositiveButton(R.string.ok_label,
@@ -56,10 +64,20 @@ public class GameActivity extends Activity {
                }
             });
       final Dialog dialog = builder.create();
-      dialog.show();
+      mHandler.postDelayed(new Runnable() {
+         @Override
+         public void run() {
+            mMediaPlayer = MediaPlayer.create(GameActivity.this,
+                  winner == Tile.Owner.X ? R.raw.oldedgar_winner
+                        : winner == Tile.Owner.O ? R.raw.notr_loser
+                        : R.raw.department64_draw
+            );
+            mMediaPlayer.start();
+            dialog.show();
+         }
+      }, 500);
 
-      // Reset the board to the initial position
-      mGameFragment.initGame();
+      mGameFragment.initGame();  // Reset the board to the initial position
    }
 
    public void startThinking() {
@@ -73,8 +91,20 @@ public class GameActivity extends Activity {
    }
 
    @Override
+   protected void onResume() {
+      super.onResume();
+      mMediaPlayer = MediaPlayer.create(this, R.raw.frankum_loop001e);
+      mMediaPlayer.setLooping(true);
+      mMediaPlayer.start();
+   }
+
+   @Override
    protected void onPause() {
       super.onPause();
+      mHandler.removeCallbacks(null);
+      mMediaPlayer.stop();
+      mMediaPlayer.reset();
+      mMediaPlayer.release();
       String gameData = mGameFragment.getState();
       getPreferences(MODE_PRIVATE).edit()
             .putString(PREF_RESTORE, gameData)
--- app/src/main/java/org/example/tictactoe/GameFragment.java
+++ app/src/main/java/org/example/tictactoe/GameFragment.java
@@ -9,6 +9,8 @@
 package org.example.tictactoe;
 
 import android.app.Fragment;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -21,7 +23,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class GameFragment extends Fragment {
-   // Data structures go here...
    static private int mLargeIds[] = {R.id.large1, R.id.large2, R.id.large3,
          R.id.large4, R.id.large5, R.id.large6, R.id.large7, R.id.large8,
          R.id.large9,};
@@ -29,12 +30,15 @@ public class GameFragment extends Fragment {
          R.id.small4, R.id.small5, R.id.small6, R.id.small7, R.id.small8,
          R.id.small9,};
    private Handler mHandler = new Handler();
-   // ...
    private Tile mEntireBoard = new Tile(this);
    private Tile mLargeTiles[] = new Tile[9];
    private Tile mSmallTiles[][] = new Tile[9][9];
    private Tile.Owner mPlayer = Tile.Owner.X;
    private Set<Tile> mAvailable = new HashSet<Tile>();
+   private int mSoundX, mSoundO, mSoundMiss, mSoundRewind;
+   private SoundPool mSoundPool;
+   private float mVolume = 1f;
+   // ...
    private int mLastLarge;
    private int mLastSmall;
 
@@ -44,6 +48,11 @@ public class GameFragment extends Fragment {
       // Retain this fragment across configuration changes.
       setRetainInstance(true);
       initGame();
+      mSoundPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
+      mSoundX = mSoundPool.load(getActivity(), R.raw.sergenious_movex, 1);
+      mSoundO = mSoundPool.load(getActivity(), R.raw.sergenious_moveo, 1);
+      mSoundMiss = mSoundPool.load(getActivity(), R.raw.erkanozan_miss, 1);
+      mSoundRewind = mSoundPool.load(getActivity(), R.raw.joanne_rewind, 1);
    }
 
    private void clearAvailable() {
@@ -86,8 +95,11 @@ public class GameFragment extends Fragment {
                @Override
                public void onClick(View view) {
                   if (isAvailable(smallTile)) {
+                     mSoundPool.play(mSoundX, mVolume, mVolume, 1, 0, 1f);
                      makeMove(fLarge, fSmall);
                      think();
+                  } else {
+                     mSoundPool.play(mSoundMiss, mVolume, mVolume, 1, 0, 1f);
                   }
                }
             });
@@ -106,9 +118,12 @@ public class GameFragment extends Fragment {
                int move[] = new int[2];
                pickMove(move);
                if (move[0] != -1 && move[1] != -1) {
+                  // ...
                   switchTurns();
+                  mSoundPool.play(mSoundO, mVolume, mVolume, 1, 0, 1f);
                   makeMove(move[0], move[1]);
                   switchTurns();
+                  // ...
                }
             }
             ((GameActivity) getActivity()).stopThinking();
@@ -174,6 +189,8 @@ public class GameFragment extends Fragment {
    }
 
    public void restartGame() {
+      mSoundPool.play(mSoundRewind, mVolume, mVolume, 1, 0, 1f);
+      // ...
       initGame();
       initViews(getView());
       updateAllTiles();
